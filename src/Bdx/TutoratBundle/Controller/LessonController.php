ca64c601bff8c8eeeb188e23b9d9d5004849dc29
Antoine Lucas <antoine.lucas.33@gmail.com>
2011-12-21 20:56:11 +0100

    Add lesson creation form

--- src/Bdx/TutoratBundle/Controller/LessonController.php
+++ src/Bdx/TutoratBundle/Controller/LessonController.php
@@ -24,15 +24,38 @@ class LessonController extends Controller
 
 	public function newAction(Request $request)
 	{
+		$successMessage = null;
+		$errorMessage = null;
+
 		$lesson = new Lesson();
 		$form = $this->createForm(new NewLessonType(), $lesson);
 
 		if ($request->getMethod() == 'POST') {
 			$form->bindRequest($request);
+
+			if ($form->isValid()) {
+				$em = $this->getDoctrine()->getEntityManager();
+				$em->persist($lesson);
+				$em->flush();
+
+				return $this->redirect($this->generateUrl(
+							'BdxTutoratBundle_lesson_new',
+							array(
+								'valid' => 1,
+								)));
+			} else {
+				$errorMessage = 'There is somme error';
+			}
+		}
+
+		if ($request->query->get('valid')) {
+			$successMessage = 'Bravo';
 		}
 
 		return $this->render('BdxTutoratBundle:Lesson:new.html.twig', array(
 					'form' => $form->createView(),
+					'successMessage' => $successMessage,
+					'errorMessage' => $errorMessage,
 					));
 	}
 }
