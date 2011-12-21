8a0df3c78f126b196ceb1af930007e4c800cce3a
Antoine Lucas <antoine.lucas.33@gmail.com>
2011-12-21 01:25:40 +0100

    Add first form + test with assetics

--- src/Bdx/TutoratBundle/Controller/DefaultController.php
+++ src/Bdx/TutoratBundle/Controller/DefaultController.php
@@ -3,13 +3,32 @@
 namespace Bdx\TutoratBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Bdx\TutoratBundle\Entity\Lesson;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class DefaultController extends Controller
 {
-    
-    public function indexAction($name)
-    {
-        return $this->render('BdxTutoratBundle:Default:index.html.twig', array('name' => $name));
-    }
+
+	public function indexAction($name)
+	{
+		return $this->render('BdxTutoratBundle:Default:index.html.twig', array('name' => $name));
+	}
+
+	public function lessonAction(Request $request)
+	{
+		$lesson = new Lesson();
+		$form = $this->createFormBuilder($lesson)
+			->add('name', 'text')
+			->add('information', 'textarea')
+			->getForm();
+
+		if ($request->getMethod() == 'POST') {
+			$form->bindRequest($request);
+		}
+
+		return $this->render('BdxTutoratBundle:Default:lesson.html.twig', array(
+					'form' => $form->createView(),
+					));
+	}
 }
