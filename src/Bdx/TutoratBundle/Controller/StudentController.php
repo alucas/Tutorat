62f76e71378b403112ac0d5a16025034d6f15829
Antoine Lucas <antoine.lucas.33@gmail.com>
2011-12-23 19:25:50 +0100

    Add Student crud

---
+++ src/Bdx/TutoratBundle/Controller/StudentController.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace Bdx\TutoratBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Bdx\TutoratBundle\Entity\Student;
+use Bdx\TutoratBundle\Form\StudentType;
+
+/**
+ * Student controller.
+ *
+ */
+class StudentController extends Controller
+{
+    /**
+     * Lists all Student entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entities = $em->getRepository('BdxTutoratBundle:Student')->findAll();
+
+        return $this->render('BdxTutoratBundle:Student:index.html.twig', array(
+            'entities' => $entities
+        ));
+    }
+
+    /**
+     * Finds and displays a Student entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entity = $em->getRepository('BdxTutoratBundle:Student')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Student entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('BdxTutoratBundle:Student:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+
+        ));
+    }
+
+    /**
+     * Displays a form to create a new Student entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Student();
+        $form   = $this->createForm(new StudentType(), $entity);
+
+        return $this->render('BdxTutoratBundle:Student:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView()
+        ));
+    }
+
+    /**
+     * Creates a new Student entity.
+     *
+     */
+    public function createAction()
+    {
+        $entity  = new Student();
+        $request = $this->getRequest();
+        $form    = $this->createForm(new StudentType(), $entity);
+        $form->bindRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('student_show', array('id' => $entity->getId())));
+            
+        }
+
+        return $this->render('BdxTutoratBundle:Student:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView()
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Student entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entity = $em->getRepository('BdxTutoratBundle:Student')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Student entity.');
+        }
+
+        $editForm = $this->createForm(new StudentType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('BdxTutoratBundle:Student:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Edits an existing Student entity.
+     *
+     */
+    public function updateAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entity = $em->getRepository('BdxTutoratBundle:Student')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Student entity.');
+        }
+
+        $editForm   = $this->createForm(new StudentType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        $request = $this->getRequest();
+
+        $editForm->bindRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('student_edit', array('id' => $id)));
+        }
+
+        return $this->render('BdxTutoratBundle:Student:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Student entity.
+     *
+     */
+    public function deleteAction($id)
+    {
+        $form = $this->createDeleteForm($id);
+        $request = $this->getRequest();
+
+        $form->bindRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getEntityManager();
+            $entity = $em->getRepository('BdxTutoratBundle:Student')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Student entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('student'));
+    }
+
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/Bdx/TutoratBundle/Form/StudentType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Bdx\TutoratBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+class StudentType extends AbstractType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder
+            ->add('name')
+			->add('email', null, array('required' => false));
+			->add('tel', null, array('required' => false));
+			->add('information', null, array('required' => false));
+        ;
+    }
+
+    public function getName()
+    {
+        return 'bdx_tutoratbundle_studenttype';
+    }
+
+	public function getDefaultOptions(array $options)
+	{
+	    return array(
+		        'data_class' => 'Bdx\TutoratBundle\Entity\Student',
+				);
+	}
+}
