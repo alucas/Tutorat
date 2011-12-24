b6d22e705f2ec322c379e657991a59ef25ec3a53
Antoine Lucas <antoine.lucas.33@gmail.com>
2011-12-24 16:40:48 +0100

    Starting firewall configuration

--- src/Bdx/TutoratBundle/Controller/DefaultController.php
+++ src/Bdx/TutoratBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Bdx\TutoratBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
 
 
 class DefaultController extends Controller
@@ -12,6 +13,20 @@ class DefaultController extends Controller
 	{
 		return $this->render('BdxTutoratBundle:Default:index.html.twig', array('name' => $name));
 	}
+
+    public function loginAction()
+    {
+        if ($this->get('request')->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $this->get('request')->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $this->get('request')->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render('BdxTutoratBundle:Default:login.html.twig', array(
+            'last_username' => $this->get('request')->getSession()->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error,
+        ));
+    }
 }
 
 ?>
