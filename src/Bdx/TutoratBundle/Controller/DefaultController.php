002365cb9e69b7f10afceda3dbcf52813b7a8247
Antoine Lucas <antoine.lucas.33@gmail.com>
2011-12-25 19:41:51 +0100

    Clean old loggin forms

--- src/Bdx/TutoratBundle/Controller/DefaultController.php
+++ src/Bdx/TutoratBundle/Controller/DefaultController.php
@@ -13,20 +13,6 @@ class DefaultController extends Controller
 	{
 		return $this->render('BdxTutoratBundle:Default:index.html.twig', array('name' => $name));
 	}
-
-    public function loginAction()
-    {
-        if ($this->get('request')->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $this->get('request')->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
-        } else {
-            $error = $this->get('request')->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
-        }
-
-        return $this->render('BdxTutoratBundle:Default:login.html.twig', array(
-            'last_username' => $this->get('request')->getSession()->get(SecurityContext::LAST_USERNAME),
-            'error'         => $error,
-        ));
-    }
 }
 
 ?>
