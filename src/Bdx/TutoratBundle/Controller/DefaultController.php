8e7151266d7547acb75493d60e89c81aaf4ab6b8
Antoine Lucas <antoine.lucas.33@gmail.com>
2011-12-29 05:17:24 +0100

    Add show tutor's lessons

--- src/Bdx/TutoratBundle/Controller/DefaultController.php
+++ src/Bdx/TutoratBundle/Controller/DefaultController.php
@@ -8,10 +8,34 @@ use Symfony\Component\Security\Core\SecurityContext;
 
 class DefaultController extends Controller
 {
-
 	public function indexAction($name)
 	{
 		return $this->render('BdxTutoratBundle:Default:index.html.twig', array('name' => $name));
+	}
+
+	public function showLessonsAction()
+	{
+        $em = $this->getDoctrine()->getEntityManager();
+		$tutors = $em->getRepository('BdxTutoratBundle:Tutor')
+		                      ->findAll();
+		$lessons = $em->getRepository('BdxTutoratBundle:Lesson')
+		                      ->findAll();
+
+		$getLessonId = function($lesson)
+		{
+			return $lesson->getId();
+		};
+
+		$tutorsLessons = array();
+		foreach ($tutors as $tutor) {
+			$tutorsLessons[$tutor->getId()] = $tutor->getLessons()->map($getLessonId);
+		}
+
+		return $this->render('BdxTutoratBundle:Default:showLessons.html.twig', array(
+				'tutors' => $tutors,
+				'lessons' => $lessons,
+				'tutorsLessons' => $tutorsLessons,
+				));
 	}
 }
 
